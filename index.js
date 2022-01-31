a83be76c202320404d0729f11095d7f660300b2e
Anthony Iroegbu <anthony@crenettechlabs.com>
2022-01-31 14:01:51 +0100

    using the pipe function to connect a readle stream to a writable stream

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const fs = require('fs')
 const results = []
 
 fs.createReadStream('kepler_data.csv')
+    .pipe(parse({
+        comment: '#',
+        columns: true
+    }))
     .on('data', (data) => {
         results.push(data)
     })
@@ -13,5 +17,5 @@ fs.createReadStream('kepler_data.csv')
     })
     .on('end', () => {
         console.log(results);
-        console.log('Parsing Done');
+        console.log('Done Parsing...');
     })
